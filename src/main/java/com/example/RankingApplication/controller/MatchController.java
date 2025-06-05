d93d7af547b8de46b380740574e4f96a35231214
Jacinto <jaclopher@outlook.com>
2025-06-05 13:16:00 +0100

    Added website to cross

--- src/main/java/com/example/RankingApplication/controller/MatchController.java
+++ src/main/java/com/example/RankingApplication/controller/MatchController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://daafgo.github.io/Kanarias-open/"})
 @Slf4j
 public class MatchController {
 
